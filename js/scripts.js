02f8798db7d2d2130e075089b7413d1cc40b3e70
johnnymayer <johnnymayer@gmail.com>
2018-01-09 19:03:25 -0800

    Add adjective field to form.

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput = $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
+    var adjectiveInput = $("input#adjective").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
+    $(".adjective").text(adjectiveInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+
+  });
+});
